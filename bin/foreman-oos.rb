1fe34f5b06005dc11de9d99d8e69392fb2ecb728
Anders Vaage <anders@unix.uib.no>
2019-03-04 13:44:35 +0100

    added foreman checks

---
+++ bin/foreman-oos.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+require 'sensu-plugin/check/cli'
+require 'rest-client'
+require 'json'
+
+class ForemanOutOfSync < Sensu::Plugin::Check::CLI
+  option :endpoint,
+         short: '-e URL',
+         long: '--endpoint URL',
+         default: 'https://localhost/api'
+
+  option :user,
+         short: '-u USER',
+         long: '--user USER',
+         default: 'admin'
+
+  option :password,
+         short: '-p PASSWORD',
+         long: '--password PASSWORD',
+         default: 'changeme'
+
+  option :password,
+         short: '-p PASSWORD',
+         long: '--password PASSWORD',
+         default: 'changeme'
+
+  option :verify_ssl,
+         short: '-v',
+         long: '--verify-ssl',
+         default: false
+
+  option :warning,
+         short: '-w NUM',
+         long: '--warning NUM',
+         description: 'Warning if NUM hosts are failing',
+         default: 1
+
+  option :critical,
+         short: '-c NUM',
+         long: '--critical NUM',
+         description: 'Warning if NUM hosts are failing',
+         default: 10
+
+  def get_oos(username, password, url, verify_ssl)
+    begin
+      client = RestClient::Resource.new(
+        url,
+        user: username,
+        password: password,
+        headers: { accept: :json },
+        verify_ssl: verify_ssl
+      )
+      results = client['hosts'].get(
+        params: {
+          per_page: 10_000,
+          search: "last_report < \"65 minutes ago\"
+                      and status.enabled = true"
+        }
+      )
+      hostnames = JSON.parse(results)['results'].map { |h| h['name'] }
+      return hostnames.count
+    rescue Exception => e
+      critical "Cannot connect to Foreman API: #{e}"
+    end
+  end
+
+  def run
+    count = get_oos(config[:user],
+                    config[:password],
+                    config[:endpoint],
+                    config[:verify_ssl])
+    if count > config[:warning]
+      warning "#{count} hosts are out of sync"
+    elsif count > config[:critical]
+      critical "#{count} hosts are out of sync"
+    else
+      ok 'We are within the threshold'
+    end
+  end
+end
